7a9327c5a3394b7a400cc259848b65b58727a817
Aswathi-6 <aswathimurugesan2406@gmail.com>
2025-03-07 11:27:19 +0530

    Add files via upload

---
+++ Day2_pro.java
@@ -0,0 +1,71 @@
+package day2_program;
+
+import java.util.Scanner;
+
+public class Day2_pro {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+//		int y=sc.nextInt();
+//		int given_year=1900;
+//		int ry=0;
+//		int ly=0;
+//		y=(y-1)-given_year;
+//		ly=y/4;
+//		ry=y-ly;
+//		int totaldays=(ry*365)+(ly*366)+1;
+//		int day=totaldays%7;
+//		if(day==0)
+//			System.out.println("monday");
+//		else if(day==1)
+//			System.out.println("tuesday");
+//		else if(day==2)
+//			System.out.println("wed");
+//		else if(day==3)
+//			System.out.println("thur");
+//		else if(day==4)
+//			System.out.println("fri");
+//		else if(day==5)
+//			System.out.println("sat");
+//		else if(day==6)
+//			System.out.println("sun");
+//		else
+//			System.out.println("not match");
+		
+		
+	//lucas	
+//		int a=0;
+//		int b=0;
+//		int c=1;
+//		int n = 8; // Number of terms to generate
+//
+//        
+//        for (int i = 0; i < n; i++) {
+//            System.out.print(a + " ");
+//            int next=b+c;
+//            a = b;
+//            b = c;
+//            c=next;
+            
+		
+//		int a=sc.nextInt();
+//		int b=a/10;
+//		int middle=b%10;
+//		if(middle%3==0) {
+//			System.out.println(a+" is a trendy number");
+//        }
+//		else {
+//			System.out.println(a+" not a trendy number");
+//		}
+		
+//special number
+		int m=;
+		int n=endingrange;
+		int a=10;
+		for(i=m;i<10;i++)
+		{
+			
+		}
+	}
+
+}
