7b90cfd21060636495702d3d2730ead37a3abc2a
Aswathi-6 <aswathimurugesan2406@gmail.com>
2025-03-07 11:28:53 +0530

    Add files via upload

---
+++ Day4_Pro.java
@@ -0,0 +1,136 @@
+package day4_program;
+
+import java.util.Scanner;
+
+public class Day4_Pro {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+//		String str=sc.nextLine();
+//		String str1=str.toLowerCase();
+//		int count=0;
+//		for(int i=0;i<str.length();i++) {
+//			if(str1.charAt(i)=='a'||str1.charAt(i)=='e'||str1.charAt(i)=='i'||str1.charAt(i)=='o'||str1.charAt(i)=='u') {
+//				count++;
+//			}
+//		}
+//		System.out.println(count);
+
+		
+//		String str=sc.nextLine();
+//		int l=str.length();
+//		int p=0;
+//		if(l%2==0) {
+//			p=l/2-1;
+//			System.out.println(str.charAt(p)+" "+str.charAt(p+1));
+//			System.out.println(p+" "+p+1);
+//		}
+//		else {
+//			p=l/2;
+//			System.out.println(str.charAt(p));
+//			System.out.println(p);
+//		}
+	
+//	    String s=sc.nextLine();
+//	    String a="";
+//	    for(int i=s.length()-1;i>=0;i--) {
+//	    	a=a+s.charAt(i);	    }
+//	    System.out.println(s);
+//	    if(s.equals(a)) {
+//    		System.out.println(s+" palindrome");
+//    	}
+//    	else {
+//    		System.out.println(s+" not");
+//    	}
+		
+		
+    //delete the space using replace()
+//		String s=sc.nextLine();
+//		String a=s.replace(" ","");
+//		System.out.println(a);
+	
+       
+//		String s1=sc.nextLine();
+//		String s2=sc.nextLine();
+//		System.out.println(s1.compareTo(s2));
+		                      //lexicographically not use method
+//		String s1=sc.nextLine();
+//		String s2=sc.nextLine();
+//		String s3=s1.toLowerCase();
+//		String s4=s2.toLowerCase();
+//		char ch1[]=s1.toCharArray();
+//		char ch2[]=s2.toCharArray();
+//		int result=0;
+//		for(int i=0;i<ch1.length;i++) {
+//			if(ch1[i]!=ch2[i]) {
+//				result=(int)ch1[i]-(int)ch2[i];
+//				break;
+//			}
+//		}
+//		System.out.println(result);
+		
+		
+		//email address valid or not
+//		String email=sc.nextLine();
+//		System.out.println(email);
+//		if(email.contains("com")||email.contains("net")||email.contains("in")||email.contains("org")) {
+//			System.out.println("valid email address");
+//		}
+//		else {
+//			System.out.println("invalid email address");
+//		}
+		
+		
+		//alternative store the array-xyxyxy(yes)
+//		String s=sc.nextLine();
+//		for(int i=0;i<s.length();i++) {
+//			if(s.charAt(i)==s.charAt(i+2)&&s.charAt(i+1)==s.charAt(i+3)) {
+//				System.out.println("yes");
+//				break;
+//			}
+//			else {
+//				System.out.println("no");
+//				break;
+		
+//			}
+//		}
+		
+		
+		//2nd method
+//		String s=sc.nextLine();
+//		char x=s.charAt(0);//x=a
+//		char y=s.charAt(1);//y=b
+//		int flag=0;
+//		if(x==y) {
+//		    flag=1;
+//		}
+//		else {
+//			for(int i=2;i<s.length();i++) {
+//				if(i%2==0) {
+//					if(x!=s.charAt(i)) {
+//						flag=1;
+//						break;
+//					}
+//				}
+//				else if(y!=s.charAt(i)) {
+//					flag=1;
+//					break;
+//				}
+//			}
+//			if(flag==1) {
+//				System.out.println("no");
+//			}
+//			else {
+//				System.out.println("yes");
+//			}
+//		}
+		
+		//first letter uppercase
+		String s=sc.nextLine();
+		char x=s.charAt(i);
+		String s1=.toUpperCase(0);
+		for(int i=0)
+	}
+		
+	
+}
